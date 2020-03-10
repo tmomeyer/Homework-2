7718a40118814509d29af2a12aab88cc13cf35a5
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-03-10 12:24:14 -0400

    hbjb

--- Homework-2/ViewController.swift
+++ Homework-2/ViewController.swift
@@ -9,7 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var namefield : String = "Hello I am <Name> and I really love the color <color>. Dr. Messnar will devour <Type of Food> and he will always have sypathy for <Type of Animal>. Dr. Messner enjoys to shoot his favorite <Weapon>."
+    
+    
+    @IBOutlet weak var Name: UITextField!
+    @IBOutlet weak var Color: UITextField!
+    @IBOutlet weak var Food: UITextField!
+    @IBOutlet weak var Animal: UITextField!
+    @IBOutlet weak var Weapon: UITextField!
+    @IBOutlet weak var Outputtext: UITextView!
+    @IBAction func Button(_ sender: Any) {
+        Outputtext.text = namefield.replacingOccurrences(of:"<Name>",with: Name.text!)
+        Outputtext.text =
+    Outputtext.text.replacingOccurrences(of:"<color>", with: Color.text!)
+        Outputtext.text =
+            Outputtext.text.replacingOccurrences(of:"<Type of Food>", with: Food.text!)
+        Outputtext.text =
+            Outputtext.text.replacingOccurrences(of:"<Type of Animal>", with: Animal.text!)
+        Outputtext.text =
+        Outputtext.text.replacingOccurrences(of:"<Weapon>", with: Weapon.text!)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
